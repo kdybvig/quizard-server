54bc7cb57cb69afd7bd85947aca02b981a6f4488
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-10-01 10:26:31 -0500

    fixed get route for showing quizzes by username

--- routes/quizzesRouter.js
+++ routes/quizzesRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {showQuizzes, addQuiz, updateQuiz} = require('../controllers/quizzesController');
+const {showQuizzes, showQuizzesByUser, addQuiz, updateQuiz} = require('../controllers/quizzesController');
 const quizzesRouter = express.Router();
 
 const bodyParser = require('body-parser');
 quizzesRouter.use(bodyParser.json());
 
 quizzesRouter.get('/', showQuizzes);
-quizzesRouter.get('/user/:username')
+quizzesRouter.get('/user/:username', showQuizzesByUser)
 quizzesRouter.post('/', addQuiz);
 quizzesRouter.put('/:quizid', updateQuiz);
 
